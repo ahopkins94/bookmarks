51c31ed9d2c9d1f294211ff98abf67565fca241a
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-23 15:53:12 +0100

    Introduced tableplus and added a new url

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -6,11 +6,4 @@ class Bookmark
     result = conn.exec( "SELECT * FROM bookmarks" )
     result.map { |bookmark| bookmark['url'] }
   end
-
-    # [
-    #   "http://www.makersacademy.com",
-    #   "http://www.destroyallsoftware.com",
-    #   "http://www.google.com"
-    #  ]
-
 end
